37efff83554c189b2c0e37ef99a3cf8b302ccb3b
fatdes <cktat0@gmail.com>
2025-02-19 13:16:55 +0800

    fix: simple ts supports

---
+++ pkg/grep/grep_go.go
@@ -0,0 +1,110 @@
+package grep
+
+import (
+	"bytes"
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"io/fs"
+	"strings"
+
+	"github.com/fatih/structtag"
+)
+
+type GrepGo struct {
+	*Grep
+}
+
+func NewGrepGo(grep *Grep) *GrepGo {
+	return &GrepGo{
+		grep,
+	}
+}
+
+func (g *GrepGo) Execute() error {
+	return fs.WalkDir(g.fs, ".", func(path string, info fs.DirEntry, _ error) error {
+		if info.IsDir() {
+			return nil
+		}
+
+		name := info.Name()
+		if !strings.HasSuffix(name, ".go") || strings.HasSuffix(name, "_test.go") {
+			return nil
+		}
+
+		fset := token.NewFileSet()
+		f, err := parser.ParseFile(fset, path, nil, 0)
+		if err != nil {
+			panic(err)
+		}
+
+		b := &bytes.Buffer{}
+		fmt.Fprintf(b, "# ./%s\n", path)
+
+		hasStructWithEnvTag := false
+		for _, node := range f.Decls {
+			switch node.(type) {
+
+			case *ast.GenDecl:
+				genDecl := node.(*ast.GenDecl)
+				for _, spec := range genDecl.Specs {
+					switch spec.(type) {
+					case *ast.TypeSpec:
+						typeSpec := spec.(*ast.TypeSpec)
+
+						bb := &bytes.Buffer{}
+						fmt.Fprintf(bb, "## %s\n", typeSpec.Name.Name)
+
+						hasEnvTag := false
+						switch typeSpec.Type.(type) {
+						case *ast.StructType:
+							structType := typeSpec.Type.(*ast.StructType)
+							for _, field := range structType.Fields.List {
+								if field.Tag == nil {
+									continue
+								}
+
+								tags, err := structtag.Parse(strings.Trim(field.Tag.Value, "`"))
+								if err != nil {
+									panic(err)
+								}
+
+								envValue := ""
+								envTag, err := tags.Get("env")
+								if err != nil || envTag == nil || envTag.Value() == "" {
+									continue
+								}
+								envValue = envTag.Value()
+								if strings.Contains(envValue, ",") {
+									envValue = strings.Split(envValue, ",")[0]
+								}
+
+								envDefaultValue := ""
+								envDefaultTag, err := tags.Get("envDefault")
+								if err == nil {
+									envDefaultValue = envDefaultTag.Value()
+								}
+
+								fmt.Fprintf(bb, "%s=%s\n", envValue, envDefaultValue)
+								hasEnvTag = true
+							}
+						}
+
+						if hasEnvTag {
+							b.WriteString(bb.String())
+							hasStructWithEnvTag = true
+						}
+					}
+				}
+			}
+		}
+
+		if hasStructWithEnvTag {
+			g.output.WriteString(b.String())
+			g.output.Flush()
+		}
+
+		return nil
+	})
+}
---
+++ pkg/grep/grep_ts.go
@@ -0,0 +1,51 @@
+package grep
+
+import (
+	"bytes"
+	"fmt"
+	"io/fs"
+	"regexp"
+	"strings"
+)
+
+type GrepTs struct {
+	*Grep
+}
+
+func NewGrepTs(grep *Grep) *GrepTs {
+	return &GrepTs{
+		grep,
+	}
+}
+
+func (g *GrepTs) Execute() error {
+	envPattern := regexp.MustCompile(`(?m).*process\.env\.\s*([^,\s]+)(?:.*\|\|\s*([^,\s]+))?`)
+
+	return fs.WalkDir(g.fs, ".", func(path string, info fs.DirEntry, _ error) error {
+		if info.IsDir() {
+			return nil
+		}
+
+		name := info.Name()
+		if !strings.HasSuffix(name, ".config.ts") {
+			return nil
+		}
+
+		b := &bytes.Buffer{}
+		fmt.Fprintf(b, "# ./%s\n", path)
+
+		fb, err := fs.ReadFile(g.fs, path)
+		if err != nil {
+			panic(err)
+		}
+
+		for _, submatches := range envPattern.FindAllStringSubmatch(string(fb), -1) {
+			b.WriteString(fmt.Sprintf("%s=%s\n", submatches[1], submatches[2]))
+		}
+
+		g.output.WriteString(b.String())
+		g.output.Flush()
+
+		return nil
+	})
+}
