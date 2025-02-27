92dd85d3cc68684b9635ecb7f5526174318ca09b
fatdes <cktat0@gmail.com>
2025-02-27 16:50:10 +0800

    fix: udpate to fits nestjs usage

--- pkg/grep/grep_ts.go
+++ pkg/grep/grep_ts.go
@@ -21,31 +21,41 @@ func NewGrepTs(grep *Grep) *GrepTs {
 func (g *GrepTs) Execute() error {
 	envPattern := regexp.MustCompile(`(?m).*process\.env\.\s*([-a-zA-Z_0-9]+)(?:.*(?:\|\||\?\?)\s*([^,)\s]+))?`)
 
-	return fs.WalkDir(g.fs, ".", func(path string, info fs.DirEntry, _ error) error {
-		if info.IsDir() {
+	walkFunc := func(path string, info fs.DirEntry, _ error) error {
+		if info == nil || info.IsDir() {
 			return nil
 		}
 
 		name := info.Name()
-		if !strings.HasSuffix(name, ".config.ts") {
+		if name != "config.ts" && !strings.HasSuffix(name, ".config.ts") {
 			return nil
 		}
-
-		b := &bytes.Buffer{}
-		fmt.Fprintf(b, "# ./%s\n", path)
 
 		fb, err := fs.ReadFile(g.fs, path)
 		if err != nil {
 			panic(err)
 		}
 
+		b := &bytes.Buffer{}
 		for _, submatches := range envPattern.FindAllStringSubmatch(string(fb), -1) {
 			b.WriteString(fmt.Sprintf("%s=%s\n", submatches[1], submatches[2]))
 		}
 
+		if b.Len() == 0 {
+			return nil
+		}
+
+		g.output.WriteString(fmt.Sprintf("# ./%s\n", path))
 		g.output.WriteString(b.String())
 		g.output.Flush()
 
 		return nil
-	})
+	}
+
+	err := fs.WalkDir(g.fs, "apps", walkFunc)
+	if err != nil {
+		return err
+	}
+
+	return fs.WalkDir(g.fs, "libs", walkFunc)
 }
